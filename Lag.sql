82a2d7a4d81dba30d081778ef783c216b397906c
Meena Athmanathan <MeenaAthmanathan@users.noreply.github.com>
2019-06-05 14:46:28 -0700

    Update Lag.sql

--- Lag.sql
+++ Lag.sql
@@ -1,12 +1,13 @@
-drop table if exists #timestamptest;
-create table #timestamptest as
-select distinct timestamp 'epoch' + imp_ts/1000 * interval '1 second' as  imprsn_ts,si,ss,rc
+--redshift temp table
+drop table if exists #lagtest;
+create table #lagtest as
+select distinct timestamp 'epoch' + imp_ts/1000 * interval '1 second' as  imprsn_ts,cookie_id,session_id,request_id
 from impression_detail 
 order by imp_ts
 limit 100;
 
-select si,ss,rc,imprsn_ts
+select cookie_id,session_id,request_id,imprsn_ts
 --lag sets earliest record to null, next record is compared with previous record
-,imprsn_ts - lag(imprsn_ts) over(partition by si order by imprsn_ts asc) as duration_since_last_rq
-from #timestamptest
-order by si,ss,imprsn_ts
+,imprsn_ts - lag(imprsn_ts) over(partition by cookie_id order by imprsn_ts asc) as duration_since_last_rq
+from #lagtest
+order by cookie_id,session_id,imprsn_ts
