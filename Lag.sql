8e1db724ddb44ac6fab5081d17ec1ea0b547540a
Meena Athmanathan <MeenaAthmanathan@users.noreply.github.com>
2019-06-05 14:43:17 -0700

    Update Lag.sql

--- Lag.sql
+++ Lag.sql
@@ -6,7 +6,7 @@ order by imp_ts
 limit 100;
 
 select si,ss,rc,imprsn_ts
-//lag sets earliest record to null, next record is compared with previous record
+--lag sets earliest record to null, next record is compared with previous record
 ,imprsn_ts - lag(imprsn_ts) over(partition by si order by imprsn_ts asc) as duration_since_last_rq
 from #timestamptest
 order by si,ss,imprsn_ts
