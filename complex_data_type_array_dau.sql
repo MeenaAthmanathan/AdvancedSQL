d4fd0b21c94deeffd26004f0e69fa165c145b870
Meena Athmanathan <MeenaAthmanathan@users.noreply.github.com>
2019-06-08 15:13:40 -0700

    Update complex_data_type_array_dau.sql

--- complex_data_type_array_dau.sql
+++ complex_data_type_array_dau.sql
@@ -1,7 +1,7 @@
 select 
 sum(case when dau_array[0] == 1 then 1 else 0 end) as total_new_count
 ,sum(case when dau_array[1] == 1 then 1 else 0 end) as total_recent_count
-,sum(case when dau_array[2] == 1 then 1 else 0 end) as total_old_count
+,sum(case when dau_array[3] == 1 then 1 else 0 end) as total_old_count
 from
 (SELECT  
 user_id
